d1e086e2ab398c512a8cedadac7a5b4db4b78e41
George Herrada Farfán <104341274+gherrada22@users.noreply.github.com>
2025-05-23 15:04:23 +0000

    cambios minimos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,19 +13,16 @@ const About: React.FC = () => {
         </div>
         
         <div className="grid md:grid-cols-2 gap-12 items-center">
-          <div className="relative">
-            <div className="relative z-10 rounded-lg overflow-hidden shadow-xl">
+            <div className="relative w-[500px] h-[600px] mx-auto">
+              <div className="relative z-10 rounded-lg overflow-hidden shadow-xl w-full h-full">
               <img 
-          src="/img/history.png" 
-          alt="NutriBlitz Founders" 
-          className="w-full h-auto max-w-lg mx-auto"
+                src="/img/history.png" 
+                alt="NutriBlitz Founders" 
+                className="object-cover w-full h-full"
               />
+              </div>
+              <div className="absolute top-0 left-0 -ml-2 -mt-2 w-full h-full bg-green-100 rounded-lg transform -rotate-3 z-0"></div>
             </div>
-            <div className="absolute top-0 left-0 -ml-4 -mt-4 w-full h-full bg-green-100 rounded-lg transform -rotate-3 z-0"></div>
-            <div className="absolute bottom-0 right-0 mb-4 mr-4 bg-white rounded-lg shadow-lg p-4">
-              <p className="text-brown-800 font-bold">Est. 2018</p>
-            </div>
-          </div>
           
           <div>
             <p className="text-brown-700 mb-6">
